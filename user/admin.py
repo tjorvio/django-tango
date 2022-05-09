4aa419ebdbc0eee1a83184cc23a690e4b8512315
JonThorGudmundsson <jt@jt.is>
2022-05-09 19:29:15 +0000

    bættið við módels í admin

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from user.models import Status, Profile, Payment, Country, Bid
 # Register your models here.
+admin.site.register(Status)
+admin.site.register(Profile)
+admin.site.register(Payment)
+admin.site.register(Country)
+admin.site.register(Bid)
+
